d5ec831aeecae66f75d3c98c750fb2ef40f1bb15
Fabricio C Zuardi <fabricio@fabricio.org>
2015-02-03 19:47:58 -0200

    adds optional "flow" typecheck task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
+/* @flow */
+(function () {
 'use strict';
-let gulp = require('gulp'),
-    del = require('del');
 
-const paths = {
+var gulp = require('gulp'),
+    del = require('del'),
+    execFile = require('child_process').execFile,
+    flow = null;
+
+try{
+    flow = require('flow-bin');
+} catch(err){
+    console.log('Flow-bin not available');
+}
+
+var paths = {
         src: {
             HTML:   './src/html'
         },
@@ -30,4 +41,27 @@ gulp.task('rebuild', gulp.series(
     'build'
 ));
 
+//type-check files with the
+// @flow
+//declaration without the _
+gulp.task('flow', function(cb){
+    if (flow){
+        var options = {
+            cwd: '.',
+            encoding: 'utf8',
+            env: process.env,
+            timeout: 0,
+            maxBuffer: 200*1024,
+            killSignal: 'SIGTERM'
+        };
+        execFile(flow, ['check'], options, function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            cb();
+        });
+    }
+});
+
 gulp.task('default', gulp.parallel('rebuild'));
+
+})();
