5e468bb8d68775617e322c0b6c48b964c40af8e3
Fabricio C Zuardi <fabricio@fabricio.org>
2015-02-03 17:23:46 -0200

    basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+let gulp = require('gulp'),
+    del = require('del');
+
+const paths = {
+        src: {
+            HTML:   './src/html'
+        },
+        build: {
+            ROOT:   './dist',
+            WWW:    './dist/www'
+        }
+};
+
+gulp.task('clean', function (cb){
+    del(paths.build.ROOT +'/**', cb);
+});
+
+gulp.task('html', function(){
+    gulp.src(paths.src.HTML + '/**')
+        .pipe(gulp.dest(paths.build.WWW));
+});
+
+gulp.task('build', gulp.parallel(
+    'html'
+));
+
+gulp.task('rebuild', gulp.series(
+    'clean',
+    'build'
+));
+
+gulp.task('default', gulp.parallel('rebuild'));
