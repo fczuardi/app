60dd969fe8f08bfd11661400f6aea15977951217
Fabricio C Zuardi <fabricio@fabricio.org>
2015-02-05 20:24:03 -0200

    includes transpiled dir contents and includes transpile task after npm test

---
+++ transpiled/components/helloMessage.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+class HelloMessage extends React.Component {
+  render() {
+    return React.createElement("div", null, "Hello ", this.props.name);
+  }
+}
+
+module.exports = HelloMessage;
