a6dd1ab80deb1b7ee223ca7a51cfc21454c113ff
Fabricio C Zuardi <fabricio@fabricio.org>
2015-02-06 19:12:52 -0200

    replaces separated img and html tasks with a single copy task

---
+++ tasks/copy.js
@@ -0,0 +1,20 @@
+'use strict';
+var argv = require('minimist')(process.argv.slice(2)),
+    shell = require('shelljs'),
+    mkdir = shell.mkdir,
+    cp = shell.cp;
+
+var paths = require('./paths');
+
+// command line parameters
+// --src=html or --src=img
+var src = (argv.src === 'img') ? paths.src.IMG :
+            (argv.src === 'html') ? paths.src.HTML : null,
+    dest = (argv.src === 'img') ? paths.build.IMG :
+            (argv.src === 'html') ? paths.build.WWW : null;
+
+if (src && dest){
+    // Copy files to release dir
+    mkdir('-p', dest);
+    cp('-Rf', src +'/*', dest);
+}
