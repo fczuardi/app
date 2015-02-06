8287a46e39f9f6b1fd2df31f92adf630089c7b51
Fabricio C Zuardi <fabricio@fabricio.org>
2015-02-06 03:20:40 -0200

    adds minify task

---
+++ tasks/minify.js
@@ -0,0 +1,29 @@
+'use strict';
+var path = require('path');
+
+var shell = require('shelljs'),
+    echo    = shell.echo,
+    env     = shell.env,
+    exec    = shell.exec,
+    find    = shell.find;
+
+var paths = require('./paths');
+
+//adds node_modules/.bin to the PATH
+env.PATH += ':' + path.join(__dirname, '../','node_modules', '.bin');
+
+var uncompressed = find(paths.build.JS).filter(function(filename){
+                        return  (
+                            (filename.indexOf('.min.js') === -1) &&
+                            (filename !== paths.build.JS)
+                                );
+                    });
+
+
+uncompressed.map(function(filename){
+    var cmd = 'uglifyjs -c warnings=false -m -o ' +
+                filename.replace('.js', '.min.js') + ' ' +
+                filename;
+    echo(cmd);
+    exec(cmd);
+});
